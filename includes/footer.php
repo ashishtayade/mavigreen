f48e7cbfcea685803a48afaf67780f172ea27c55
Ashish Tayade <ashish.tayade4@gmail.com>
2022-03-13 19:42:00 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,117 @@
+<!-- footer Start -->
+<footer class="footer section gray-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 mr-auto col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<div class="logo mb-4 mt-5">
+						<img src="images/footer-logo.png" alt="" class="img-fluid">
+					</div>
+					<!-- <p>Tempora dolorem voluptatum nam vero assumenda voluptate, facilis ad eos obcaecati tenetur veritatis eveniet distinctio possimus.</p> -->
+
+					<!-- <h4 class="text-capitalize mb-3 mt-3">We are social</h4>
+					<ul class="list-inline footer-socials mt-4">
+						<li class="list-inline-item"><a href="https://www.facebook.com/themefisher"><i class="icofont-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="https://twitter.com/themefisher"><i class="icofont-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.pinterest.com/themefisher/"><i class="icofont-linkedin"></i></a></li>
+					</ul> -->
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Quick Links</h4>
+					<!-- <div class="divider mb-4"></div> -->
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Gallery</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="widget mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Products</h4>
+					<!-- <div class="divider mb-4"></div> -->
+
+					<ul class="list-unstyled footer-menu lh-35">
+						<li><a href="falcon-series.php">falcon-series</a></li>
+						<li><a href="eagle-series.php">eagle-series</a></li>
+						<li><a href="albatross-series.php">albatross-series</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="widget widget-contact mb-5 mb-lg-0">
+					<h4 class="text-capitalize mb-3">Get in Touch</h4>
+					<!-- <div class="divider mb-4"></div> -->
+
+					<div class="footer-contact-block mb-4">
+						<div class="icon d-flex align-items-center">
+							<i class="icofont-email mr-3"></i>
+							<span class="h6 mb-0">Mail us at</span>
+						</div>
+						<h4 class="mt-2"><a href="mailto:info@mavigreenenergy.com">info@mavigreenenergy.com</a></h4>
+					</div>
+
+					<div class="footer-contact-block">
+						<div class="icon d-flex align-items-center">
+							<i class="icofont-phone mr-3"></i>
+							<span class="h6 mb-0">Call / WhatsApp</span>
+						</div>
+						<h4 class="mt-2"><a href="tel:7841047245">7841047245</a></h4>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-btm py-4 mt-5">
+			<div class="row align-items-center justify-content-between">
+				<div class="col-lg-12">
+					<div class="copyright text-center">
+						&copy; Copyright Reserved to mavigreenenergy.com
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4">
+					<a class="backtop js-scroll-trigger" href="#top">
+						<i class="icofont-long-arrow-up"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+   
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+
+    
+    <!-- Main jQuery -->
+    <script src="plugins/jquery/jquery.js"></script>
+    <!-- Bootstrap 4.3.2 -->
+    <script src="plugins/bootstrap/js/popper.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="plugins/counterup/jquery.easing.js"></script>
+    <!-- Slick Slider -->
+    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+    <!-- Counterup -->
+    <script src="plugins/counterup/jquery.waypoints.min.js"></script>
+    
+    <script src="plugins/shuffle/shuffle.min.js"></script>
+    <script src="plugins/counterup/jquery.counterup.min.js"></script>
+    <!-- Google Map -->
+    <script src="plugins/google-map/map.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>    
+    
+    <script src="js/script.js"></script>
+    <script src="js/contact.js"></script>
---
+++ includes/header.php
@@ -0,0 +1,72 @@
+<header>
+	<div class="header-top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<ul class="top-bar-info list-inline-item pl-0 mb-0">
+						<li class="list-inline-item"><a href="mailto:info@mavigreenenergy.com"><i class="icofont-email mr-2"></i>info@mavigreenenergy.com</a></li>
+						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Bavdhan, Pune, Maharashtra - 411021</li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+						<a href="tel:+23-345-67890" >
+							<span>Call / WhatsApp : </span>
+							<span class="h4-1">7841047245</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+		 	 <a class="navbar-brand" href="index.php">
+			  	<img src="images/logo.png" alt="" class="img-fluid" style="height: 70px">
+			  </a>
+
+		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="icofont-navigation-menu"></span>
+		  </button>
+	  
+		  <div class="collapse navbar-collapse" id="navbarmain">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="index.php">Home</a>
+			  </li>
+			   <li class="nav-item"><a class="nav-link" href="about-us.php">About Us</a></li>
+			    <!-- <li class="nav-item"><a class="nav-link" href="service.html">Services</a></li> -->
+
+			    <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="products.php" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products <i class="icofont-thin-down" style="font-weight: 900;"></i></a>
+					<ul class="dropdown-menu" aria-labelledby="dropdown02">
+						<li><a class="dropdown-item" href="falcon-series.php">falcon-series</a></li>
+						<li><a class="dropdown-item" href="eagle-series.php">eagle-series</a></li>
+						<li><a class="dropdown-item" href="albatross-series.php">albatross-series</a></li>
+					</ul>
+			  	</li>
+
+			  	<!-- <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctors <i class="icofont-thin-down"></i></a>
+					<ul class="dropdown-menu" aria-labelledby="dropdown03">
+						<li><a class="dropdown-item" href="doctor.html">Doctors</a></li>
+						<li><a class="dropdown-item" href="doctor-single.html">Doctor Single</a></li>
+						<li><a class="dropdown-item" href="appoinment.html">Appoinment</a></li>
+					</ul>
+			  	</li>
+
+			   <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="blog-sidebar.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i class="icofont-thin-down"></i></a>
+					<ul class="dropdown-menu" aria-labelledby="dropdown05">
+						<li><a class="dropdown-item" href="blog-sidebar.html">Blog with Sidebar</a></li>
+
+						<li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
+					</ul>
+			  	</li> -->
+			   <li class="nav-item"><a class="nav-link" href="gallery.php">Gallery</a></li>
+			   <li class="nav-item"><a class="nav-link" href="contact-us.php">Contact Us</a></li>
+			</ul>
+		  </div>
+		</div>
+	</nav>
+</header>
